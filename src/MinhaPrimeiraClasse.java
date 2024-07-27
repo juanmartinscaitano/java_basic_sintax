b63c1e20c585836db323eb8d8ca1e4606fd7b136
Juan Martins Caitano <juanmartinscaitano@gmail.com>
2024-07-27 16:11:08 -0300

    refactor: removido mensagem printl

--- src/MinhaPrimeiraClasse.java
+++ src/MinhaPrimeiraClasse.java
@@ -1,5 +1,5 @@
 public class MinhaPrimeiraClasse {
     public static void main(String[] args) {
-        System.out.println("Meu código escrito com uma IDE");
+        System.out.println("");
     }
 }
