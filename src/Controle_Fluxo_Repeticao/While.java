9c1c61f4ade48ab617675901523180358048c8e2
Juan Martins Caitano <juanmartinscaitano@gmail.com>
2024-08-09 16:52:10 -0300

    feature:

--- src/Controle_Fluxo_Repeticao/While.java
+++ src/Controle_Fluxo_Repeticao/While.java
@@ -1,7 +1,23 @@
 package Controle_Fluxo_Repeticao;
 
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class While {
     public static void main(String[] args) {
+        double mesada = 50.0;
+        while (mesada > 0) {
+            Double valorDoce = valorAleatorio();
+            if(valorDoce > mesada)
+                valorDoce = mesada;
 
+            System.out.println("Doce do valor: " + valorDoce + " adicionado no carrinho");
+            mesada = mesada -valorDoce;
+        }
+        System.out.println("Mesada: " + mesada);
+        System.out.println("Anya gastou toda a sua mesada em doces");
+    }
+    private static double valorAleatorio() {
+        return ThreadLocalRandom.current().nextDouble(2, 8);
     }
 }
