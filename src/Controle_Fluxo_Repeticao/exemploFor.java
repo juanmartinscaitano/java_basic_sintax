bc0c0667e56a70a822f564933a95955a53b6a395
Juan Martins Caitano <juanmartinscaitano@gmail.com>
2024-08-08 21:40:25 -0300

    feature: adicionado o metodo aleatorio

--- src/Controle_Fluxo_Repeticao/exemploFor.java
+++ src/Controle_Fluxo_Repeticao/exemploFor.java
@@ -1,10 +1,18 @@
 package Controle_Fluxo_Repeticao;
 
+import java.util.Random;
+import java.util.random.RandomGenerator;
+
 public class exemploFor {
     public static void main(String[] args) {
         boolean dormiu = false;
-        for(int carneirinhos = 1; carneirinhos <=20; carneirinhos ++) {
+        for(int carneirinhos = 1; carneirinhos <=20; carneirinhos = carneirinhos * 3) {
             System.out.println(carneirinhos + " - carneirinho(s)");
+
+            if(new Random().nextInt(20) == carneirinhos) {
+                break;
+//            double numeroAleatorio = Math.random();
+            }
         }
 
         String alunos[] = {"jhon", "pablo", "george", "kenedy"} ;
