a98b1bdf5d98f80913a516f42bbbadb6f639af33
Juan Martins Caitano <juanmartinscaitano@gmail.com>
2024-08-10 17:55:48 -0300

    feature: adicionado try e catch no Scanner para resolver como exercicio

--- src/exeptions/AboutMe.java
+++ src/exeptions/AboutMe.java
@@ -1,5 +1,6 @@
 package exeptions;
 
+import java.util.InputMismatchException;
 import java.util.Locale;
 import java.util.Scanner;
 import java.util.Date;
@@ -9,20 +10,26 @@ public class AboutMe {
         Date date = new Date();
         System.out.println(date);
 
-        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        try {
+            Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
-        System.out.println("Digite seu nome: ");
-        String nome = scanner.next();
-        System.out.println("Digite seu sobrenome: ");
-        String sobrenome = scanner.next();
-        System.out.println("Digite sua idade: ");
-        int idade = scanner.nextInt();
-        System.out.println("Dgite sua altura: ");
-        double altura = scanner.nextDouble();
+            System.out.println("Digite seu nome: ");
+            String nome = scanner.next();
+            System.out.println("Digite seu sobrenome: ");
+            String sobrenome = scanner.next();
+            System.out.println("Digite sua idade: ");
+            int idade = scanner.nextInt();
+            System.out.println("Dgite sua altura: ");
+            double altura = scanner.nextDouble();
 
-        System.out.println("Olá, me chamo " + nome.toUpperCase() + " " + sobrenome.toUpperCase());
-        System.out.println("Tenho " + idade + " anos ");
-        System.out.println("Minha altura é " + altura + "cm ");
-        scanner.close();
+            System.out.println("Olá, me chamo " + nome.toUpperCase() + " " + sobrenome.toUpperCase());
+            System.out.println("Tenho " + idade + " anos ");
+            System.out.println("Minha altura é " + altura + "cm ");
+            scanner.close();
+        }catch (InputMismatchException ex) {
+            //faca sua logica aqui
+        }catch (Exception ex) {
+            //faca sua logica
+        }
     }
 }
