04544535346f8b22d18923139dee6c0d0829f274
Juan Martins Caitano <juanmartinscaitano@gmail.com>
2024-08-12 18:36:40 -0300

    feature: imprimindo o limite de credito

--- src/OrientacaoObjetos/Sistema.java
+++ src/OrientacaoObjetos/Sistema.java
@@ -1,2 +1,20 @@
-package OrientacaoObjetos;public class Sistema {
+package OrientacaoObjetos;
+
+public class Sistema {
+    public static void main(String[] args) {
+        Client jonas = new Client("jonas");
+        System.out.println(jonas.nome + " seu limite de crédito é: R$" + jonas.limiteCartao);
+        jonas.solicitarLimiteCredito(200.0);
+        System.out.println(jonas.nome + " seu limite de crédito é: R$" + jonas.limiteCartao);
+
+        jonas.comprar(20.0);
+        System.out.println(jonas.limiteCartao);
+
+        Client iza = new Client("iza");
+        System.out.println(iza.nome + " Seu limite de crédito é: R$" + iza.limiteCartao);
+        iza.solicitarLimiteCredito(120.0);
+        System.out.println(iza.nome + " seu limite de crédito é: R$" + iza.limiteCartao);
+        iza.comprar(42.0);
+
+    }
 }
